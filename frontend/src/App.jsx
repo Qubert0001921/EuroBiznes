9a0d7421e19bd2760bfd7c511ba5ddc80bda7e39
Qubert0001921 <kajtek5e@gmail.com>
2025-06-06 16:44:44 +0200

    added login frontend tab

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import MainTab from './tabs/MainTab'
 function App() {
     let ws = new WebSocket("ws://localhost:8000/ws")
    
-
     return (
         <>
             <MainTab websocket={ws} />
--- frontend/src/tabs/LoginTab.jsx
+++ frontend/src/tabs/LoginTab.jsx
@@ -1,7 +1,25 @@
-function LoginTab() {
+import { useState } from "react"
+import eventTypes from "../eventTypes"
+
+function LoginTab(websocket) {
+    const [login, setLogin] = useState("")
+
+    function onLogin() {
+        const jsonData={
+            eventType: eventTypes.login,
+            data: {
+                login
+            }
+        }
+        websocket.send(JSON.stringify(jsonData))
+        
+    }
+
     return (
         <>
-            
+            <label>Login: </label>
+            <input type="text" value={login}/>
+            <button onClick={onLogin}>Login</button>
         </>
     )
 }

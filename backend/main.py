863ca25eac0990800a651278ebfe12d23469787e
Kris J <104223005+KrisoMoriso@users.noreply.github.com>
2025-06-06 14:57:53 +0200

    code in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,38 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+moneyBalance: int = 0
+
+@app.get("/")
+def root():
+    return None
+
+
+clients: list[WebSocket] = []
+
+@app.websocket_route("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    global moneyBalance
+    await websocket.accept()
+    clients.append(websocket)  
+    try:
+        didNotDisconnect = True
+        while didNotDisconnect:  
+            data = await websocket.receive()
+            if data['type'] == "websocket.receive":
+                print(data)
+                money: int = int(data["text"]) 
+                moneyBalance += money
+                json_data = {
+                    "balance":moneyBalance
+                }
+                for i in clients:
+                    await i.send_json(json_data)
+            else:
+                didNotDisconnect =False
+    except WebSocketDisconnect: 
+        clients.remove(websocket)
 
